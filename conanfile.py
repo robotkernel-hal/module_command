17427b7f810c0b386c8bc546c509f53ceb8866dd
Robert Burger <robert.burger@dlr.de>
2025-11-06 11:21:38 +0100

    cleanup: build system

--- conanfile.py
+++ conanfile.py
@@ -1,11 +1,14 @@
 from conan import ConanFile
 
 class MainProject(ConanFile):
-    python_requires = "conan_template/[~=5]@robotkernel/stable"
+    python_requires = "conan_template/[~5]@robotkernel/stable"
     python_requires_extend = "conan_template.RobotkernelConanFile"
 
     name = "module_command"
     description = "robotkernel module to execute commands on trigger input."
     exports_sources = ["*", "!.gitignore"]
     requires = "robotkernel/[~6]@robotkernel/unstable"
+
+    def source(self):
+        self.run(f"sed 's/AC_INIT(.*/AC_INIT([{self.name}], [{self.version}], [{self.author}])/' configure.ac.in > configure.ac")
 
