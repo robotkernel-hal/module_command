825e6c4d010df0ad8070362ef1fd124fd7b16621
Robert Burger <robert.burger@dlr.de>
2026-05-26 11:09:06 +0200

    change: using stable deps

--- conanfile.py
+++ conanfile.py
@@ -1,12 +1,12 @@
 from conan import ConanFile
 
 class MainProject(ConanFile):
-    python_requires = "conan_template/[~6]@robotkernel/unstable"
+    python_requires = "conan_template/[~6]@robotkernel/stable"
     python_requires_extend = "conan_template.RobotkernelConanFile"
 
     name = "module_command"
     description = "robotkernel module to execute commands on trigger input."
     exports_sources = ["*", "!.gitignore"]
-    requires = "robotkernel/[~6]@robotkernel/unstable"
+    requires = "robotkernel/[~6]@robotkernel/stable"
 
 
